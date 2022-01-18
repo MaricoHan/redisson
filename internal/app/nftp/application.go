000c95506e3bf1deaf397966cea7da253ed6ba4d
Dreamer <745124335@qq.com>
2022-01-18 17:04:28 +0800

    import nft orm level

--- internal/app/nftp/application.go
+++ internal/app/nftp/application.go
@@ -1,11 +1,12 @@
 package nftp
 
 import (
+	"gitlab.bianjie.ai/irita-paas/orms/orm-nft"
+
 	"gitlab.bianjie.ai/irita-paas/open-api/config"
 	"gitlab.bianjie.ai/irita-paas/open-api/internal/app/nftp/controller"
 	"gitlab.bianjie.ai/irita-paas/open-api/internal/pkg/kit"
 	"gitlab.bianjie.ai/irita-paas/open-api/internal/pkg/log"
-	"gitlab.bianjie.ai/irita-paas/open-api/internal/pkg/mysql"
 	"gitlab.bianjie.ai/irita-paas/open-api/internal/pkg/redis"
 )
 
@@ -34,20 +35,20 @@ func (s NFTPServer) Initialize() {
 	//初始化redis连接
 	redis.Connect(conf.Redis.Address, conf.Redis.Password, conf.Redis.DB)
 	//初始化mysql连接
-	mysql.Connect(
+	orm.Connect(
 		conf.Mysql.User,
 		conf.Mysql.Password,
 		conf.Mysql.Host,
 		conf.Mysql.Port,
 		conf.Mysql.DBName,
-		mysql.DebugOption(conf.Server.LogLevel == log.DebugLevel),
-		mysql.MaxIdleConnsOption(conf.Mysql.MaxIdleConns),
-		mysql.MaxOpenConnsOption(conf.Mysql.MaxOpenConns),
-		mysql.MaxLifetimeOption(conf.Mysql.MaxLifeTime),
-		mysql.WriteOption(log.Log),
+		orm.DebugOption(conf.Server.LogLevel == log.DebugLevel),
+		orm.MaxIdleConnsOption(conf.Mysql.MaxIdleConns),
+		orm.MaxOpenConnsOption(conf.Mysql.MaxOpenConns),
+		orm.MaxLifetimeOption(conf.Mysql.MaxLifeTime),
+		orm.WriteOption(log.Log),
 	)
 }
 
 func (s NFTPServer) Stop() {
-	mysql.Close()
+	orm.CloseDB()
 }
