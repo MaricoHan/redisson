2337c01ea60389b67c88b041dd590f179ab45fdc
sxj <a550374@163.com>
2022-02-11 17:42:16 +0800

    format uri,Dynamic estimation gas,

--- internal/app/nftp/handlers/nft_class.go
+++ internal/app/nftp/handlers/nft_class.go
@@ -48,8 +48,8 @@ func (h nftClass) CreateNftClass(ctx context.Context, request interface{}) (inte
 		return nil, types.NewAppError(types.RootCodeSpace, types.ClientParamsError, "Invalid Description")
 	}
 
-	if req.Uri != "" && len([]rune(strings.TrimSpace(req.Uri))) > 256 {
-		return nil, types.NewAppError(types.RootCodeSpace, types.ClientParamsError, "Invalid Uri")
+	if err := h.base.UriCheck(req.Uri); err != nil {
+		return nil, err
 	}
 
 	if req.UriHash != "" && len([]rune(strings.TrimSpace(req.UriHash))) > 512 {
