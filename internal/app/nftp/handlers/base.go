2f818db6ba1deb83a1f551e97eaf796826ca109e
ht <22350747@qq.com>
2022-03-02 18:17:25 +0800

    更正chainId

--- internal/app/nftp/handlers/base.go
+++ internal/app/nftp/handlers/base.go
@@ -15,13 +15,13 @@ type base struct {
 }
 
 func (h base) ChainID(ctx context.Context) uint64 {
-	rec := ctx.Value("chain_id")
-	chainId, ok := rec.(string)
+	keysList := ctx.Value("X-App-Id")
+	keysListString, ok := keysList.([]string)
 	if !ok {
 		return 0
 	}
-	res, _ := strconv.ParseInt(chainId, 10, 64)
-	return uint64(res)
+	ChainID, _ := strconv.ParseInt(keysListString[0], 10, 64)
+	return uint64(ChainID)
 }
 
 func (h base) UriCheck(uri string) error {
