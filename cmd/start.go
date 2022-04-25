d1370cf096ba5ce55d5d38b3d8efeb30bc834d61
xuemei <xuemei@bianjie.ai>
2022-04-25 15:46:05 +0800

    修复读取配置文件

--- cmd/start.go
+++ cmd/start.go
@@ -1,21 +1,18 @@
 package cmd
 
 import (
-	"bytes"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"gitlab.bianjie.ai/avata/open-api/internal/app"
 	"gitlab.bianjie.ai/avata/open-api/internal/pkg/configs"
-	"io/ioutil"
 )
 
 var (
 	localConfig string
-
-	startCmd = &cobra.Command{
+	startCmd    = &cobra.Command{
 		Use:     "start",
-		Example: "start openapi server",
+		Example: "start order server",
 		Run: func(cmd *cobra.Command, args []string) {
 			run()
 		},
@@ -28,36 +25,25 @@ func init() {
 }
 
 func run() {
-	data, err := ioutil.ReadFile(localConfig)
-	if err != nil {
-		log.Error("Error: get config data error: ", err)
+	v := viper.New()
+	// Find home directory.
+	v.AddConfigPath(localConfig)
+	v.SetConfigName("cfg")
+	v.SetConfigType("toml")
+
+	// Find and read the config file
+	if err := v.ReadInConfig(); err != nil { // Handle errors reading the config file
+		log.Errorf("read config err:%s", err.Error())
 		return
 	}
-	config, err := readConfig(data)
-	if err != nil {
-		log.Error("Error: read config error: ", err)
+	var config configs.Config
+	if err := v.Unmarshal(&config); err != nil {
+		log.Errorf("unmarshal config err:%s", err.Error())
 		return
 	}
-	configs.Cfg = *config
+	configs.Cfg = config
 	app.Start(&configs.Context{
 		Logger: log.New(),
-		Config: config,
+		Config: &config,
 	})
-}
-
-func readConfig(data []byte) (*configs.Config, error) {
-	v := viper.New()
-	v.SetConfigType("toml")
-	reader := bytes.NewReader(data)
-	err := v.ReadConfig(reader)
-	if err != nil {
-		log.Error("Error: viper read config error: ", err)
-		return nil, err
-	}
-	cfg := &configs.Config{}
-	if err := v.Unmarshal(cfg); err != nil {
-		log.Error("Error: unmarshal config error: ", err)
-		return nil, err
-	}
-	return cfg, nil
 }
