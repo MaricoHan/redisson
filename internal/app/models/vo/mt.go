105183c2ff60012c03df9c46fa9336134687448d
ht <22350747@qq.com>
2022-06-15 16:56:28 +0800

    修改创建类别名称

--- internal/app/models/vo/mt.go
+++ internal/app/models/vo/mt.go
@@ -2,9 +2,9 @@ package vo
 
 type CreateMTClassRequest struct {
 	//Base
-	Name        string                 `json:"name" validate:"required"`
+	Name        string                 `json:"mt_class_name"`
 	Data        string                 `json:"data"`
-	Owner       string                 `json:"owner" validate:"required"`
+	Owner       string                 `json:"owner"`
 	Tag         map[string]interface{} `json:"tag"`
 	OperationID string                 `json:"operation_id"`
 }
