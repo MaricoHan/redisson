7e3366298a85e807d1ee978ecc4c0e2dcf629e30
ht <22350747@qq.com>
2022-08-27 10:56:31 +0800

    实现 mutex

---
+++ redisson_test.go
@@ -0,0 +1,56 @@
+package redisson_test
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+
+	"github.com/MaricoHan/redisson"
+	"github.com/MaricoHan/redisson/internal/root"
+)
+
+func TestMutex(t *testing.T) {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+	redisson := redisson.New(client, &root.Options{
+		LockTimeout: 10 * time.Second,
+	})
+
+	mutex := redisson.NewMutex("han")
+
+	err := mutex.Lock()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log("lock successfully")
+
+	// 测试：其他协程无法解锁
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(1)
+	go func() {
+		defer func() {
+			waitGroup.Done()
+		}()
+		mutex := redisson.NewMutex("han")
+		err = mutex.Unlock()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		t.Log("unlock successfully")
+	}()
+	waitGroup.Wait()
+
+	// 测试：加锁的协程可以顺利解锁
+	err = mutex.Unlock()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log("unlock successfully")
+}
