f20de9bbe569360536fb11f63ae0d08722914f81
ht <22350747@qq.com>
2023-03-06 11:27:43 +0800

    bug: 用户不传时，不应该报错

--- internal/app/handlers/base.go
+++ internal/app/handlers/base.go
@@ -117,11 +117,15 @@ func (pageBasic) NextKey(ctx context.Context) string {
 }
 
 func (p pageBasic) CountTotal(ctx context.Context) (string, error) {
-	CountTotal := p.StringValue(ctx, "count_total")
-	if CountTotal != "0" && CountTotal != "1" {
+	CountTotal := ctx.Value("count_total")
+	if CountTotal == nil {
+		return "0", nil
+	}
+
+	if CountTotal.(string) != "0" && CountTotal.(string) != "1" {
 		return "", errors2.New(errors2.ClientParams, fmt.Sprintf(common.ERR_INVALID_VALUE, "count_total"))
 	}
-	return CountTotal, nil
+	return CountTotal.(string), nil
 }
 
 func (pageBasic) StringValue(ctx context.Context, key string) string {
